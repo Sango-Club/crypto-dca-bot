46d6cbabbb129c3bdc1c153cdfd31524094629e8
Erwin Lejeune <erwin.lejeune@cm-robotics.com>
2022-09-30 00:59:56 +0200

    allow to run tests in docker compose up

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,12 @@
 
 set -e
 source .env
-cd src 
-echo "Running Python App..."
-python3 main.py
+
+if [ ${TESTING} = "true" ]
+then
+    pytest-3
+else
+    cd src 
+    echo "Running Python App..."
+    python3 main.py
+fi
